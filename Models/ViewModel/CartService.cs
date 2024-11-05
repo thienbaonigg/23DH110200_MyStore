e5809ae3dfe683baf107ac85fd4ecf792c9ced2c
Ciel <23DH110200@st.huflit.edu.vn>
2024-11-05 23:38:17 +0700

    15

---
+++ Models/ViewModel/CartService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyStore.Models.ViewModel
+{
+    public class ProductDetailsVM : Controller
+    {
+        // GET: ProductDetailsVM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
