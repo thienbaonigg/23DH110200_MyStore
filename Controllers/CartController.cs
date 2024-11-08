fd4a109d3dd61db67e145933faeb479902aa8fc6
Ciel <23DH110200@st.huflit.edu.vn>
2024-11-08 23:18:29 +0700

    t

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyStore.Models;
+using MyStore.Models.ViewModel;
+
+namespace MyStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private masterEntities db = new masterEntities();
+        private CartService GetCartService()
+        {
+            return new CartService(Session);
+        }
+        public ActionResult Index()
+        {
+            var cart = GetCartService();
+            return View(cart);
+        }
+        public ActionResult AddToCart(int id, int quantity = 1)
+        {
+            var product = db.Products.Find(id);
+            if (product == null)
+            {
+                var cartService = GetCartService();
+                cartService.GetCart().AddItem(product.ProductID, product.ProductImage,
+                    product.ProductName, product.ProductPrice, quantity, product.Category.CategoryName);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveFromCart(int id)
+        {
+            var cartService = GetCartService();
+            cartService.GetCart().RemoveItem(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult ClearCart()
+        {
+            GetCartService().ClearCart();
+            return RedirectToAction("Index");
+
+        }
+        [HttpPost]
+        public ActionResult UpdateQuantity(int id, int quantity)
+        {
+            var cartService = GetCartService();
+            cartService.GetCart().UpdateQuantity(id, quantity);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Cart.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyStore.Models.Models
+{
+    using System;
+    using System.Collections.Generic;
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage",
+            "CA2214:DoNotCallOveridavleMethodsInConstructors")]
+        public User()
+        {
+            this.Customers = new HashSet<Customer>();
+        }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserRole { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage",
+           "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
